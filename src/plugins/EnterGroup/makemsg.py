b8d535df804979b5dfd4c0deb46a66d6f4a24003
Merky <gaomengkai0@outlook.com>
2022-07-22 09:05:43 +0800

    把 进群聊天 模块单独拿出来

---
+++ src/plugins/EnterGroup/makemsg.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as ET
+
+
+def makemsg(daomeidan: str, describe: str, img_src: str) -> str:
+    msg = ET.Element("msg")
+    # set msg.property
+    msg.set("serviceID", "1")
+    msg.set("templateID", "-1")
+    msg.set("action", "web")
+    msg.set("brief", f"{daomeidan}已经进群聊天")
+    msg.set("sourceMsgId", "0")
+    msg.set("url", "")
+    msg.set("flag", "0")
+    msg.set("adverSign", "0")
+    msg.set("multiMsgFlag", "0")
+    item = ET.SubElement(msg, "item")
+    item.set("layout", "2")
+    item.set("advertiser_id", "0")
+    item.set("aid", "0")
+    pic = ET.SubElement(item, "picture")
+    pic.set("cover", img_src)
+    pic.set("w", "0")
+    pic.set("h", "0")
+    title = ET.SubElement(item, "title")
+    title.text = f"{daomeidan}已经进群聊天"
+    summary = ET.SubElement(item, "summary")
+    summary.text = describe
+    source = ET.SubElement(msg, "source")
+
+    # dump
+    xml_str = ET.tostring(msg, encoding="utf-8",
+                          xml_declaration=True).decode("utf-8")
+    # CQCODE encapsulation
+    cqcode = "[CQ:xml,data=" + xml_str + ",resid=]"
+    return cqcode
